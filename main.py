15ec43ffabb099251d668904875638d35be4c2b7
Ivan Tsvetkov <amunra23@MacBook-Pro-Ivan.local>
2023-04-22 19:57:09 +0300

    #9: Добавлена подгрузка сообщений бота из файла. Добавлен пример конфига .env.example

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 
@@ -7,22 +8,25 @@ import telebot as tb
 
 
 class TelegramBotEmptyAudienceBMSTU:
-    def __init__(self):
+    def __init__(self, bot_messages_json: dict):
         self.bot = tb.TeleBot(os.getenv("TOKEN"))
+        self.bot_messages = bot_messages_json
 
         @self.bot.message_handler(commands=['start'])
         def init_command_handler(message: tb.types.Message):
             self.init_command_process(message)
 
+        # @self.bot.message_handler(content_types=['text'])
+        # def work(message: tb.types.Message):
+
     def init_command_process(self, message: tb.types.Message):
-        self.bot.send_message(message.from_user.id,
-                              "Привет! Я бот поиска свободных аудиторий в Бауманке")
+        self.bot.send_message(message.from_user.id, self.bot_messages["HI"])
 
     def run(self):
         self.bot.infinity_polling()
 
 
-def get_logger() -> logging.Logger:
+def get_logger(log_level: str) -> logging.Logger:
     logger = logging.getLogger(__name__)
     handler = logging.StreamHandler()
 
@@ -31,13 +35,34 @@ def get_logger() -> logging.Logger:
     handler.setFormatter(formatter)
 
     logger.addHandler(handler)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(log_level)
 
     return logger
 
 
+def get_messages_from_json():
+    try:
+        with open(os.getenv("MESSAGES_FILE_PATH"), 'r') as messages_file:
+            bot_messages_json: dict = json.load(messages_file)
+    except FileNotFoundError:
+        logger.critical('Неверно указан путь до файла' +
+                        f'с сообщениями бота: {os.getenv("MESSAGES_FILE_PATH")}')
+        return None
+    except json.decoder.JSONDecodeError:
+        logger.critical('Файл с сообщениями бота поврежден')
+        return None
+    except Exception as exception:
+        logger.critical(str(exception))
+        return None
+
+    return bot_messages_json
+
+
 if __name__ == "__main__":
-    logger = get_logger()
-    load_dotenv()  # должен быть файл .env, а в нем токен
-    bot = TelegramBotEmptyAudienceBMSTU()
-    bot.run()
+    load_dotenv()  # должен быть файл .env, смотреть .env.example
+    logger = get_logger(os.getenv("LOG_LEVEL"))
+    bot_messages_json = get_messages_from_json()
+
+    if (bot_messages_json is not None):
+        bot = TelegramBotEmptyAudienceBMSTU(bot_messages_json)
+        bot.run()
