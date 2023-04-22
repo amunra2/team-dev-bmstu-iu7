07464a41b89952bda598d248c4ccf26794611a2a
Ivan Tsvetkov <amunra23@MacBook-Pro-Ivan.local>
2023-04-22 17:11:43 +0300

    #5: Бот переписан на классы

--- main.py
+++ main.py
@@ -1,36 +1,43 @@
-import os
 import logging
+import os
 
-import telebot as tb
 from dotenv import load_dotenv
 
-
-def main():
-    bot = tb.TeleBot(os.getenv("TOKEN"))
-
-    @bot.message_handler(commands=['start'])
-    def sayHi(message: tb.types.Message):
-        logger.info(f'username: {message.from_user.username}')
-        bot.send_message(message.from_user.id, "Привет! Я бот расписания поиска свободных аудиторий")
-
-    bot.infinity_polling()
+import telebot as tb
 
 
-def getLogger() -> logging.Logger:
+class TelegramBotEmptyAudienceBMSTU:
+    def __init__(self):
+        self.bot = tb.TeleBot(os.getenv("TOKEN"))
+
+        @self.bot.message_handler(commands=['start'])
+        def init_command_handler(message: tb.types.Message):
+            self.init_command_process(message)
+
+    def init_command_process(self, message: tb.types.Message):
+        self.bot.send_message(message.from_user.id,
+                              "Привет! Я бот поиска свободных аудиторий в Бауманке")
+
+    def run(self):
+        self.bot.infinity_polling()
+
+
+def get_logger() -> logging.Logger:
     logger = logging.getLogger(__name__)
     handler = logging.StreamHandler()
 
-    formatLogMessage = '%(asctime)s: (%(name)s) - %(levelname)s:\t%(message)s'
-    formatter = logging.Formatter(formatLogMessage)
+    format_log_message = '%(asctime)s: (%(name)s) - %(levelname)s:\t%(message)s'
+    formatter = logging.Formatter(format_log_message)
     handler.setFormatter(formatter)
 
     logger.addHandler(handler)
     logger.setLevel(logging.DEBUG)
-    
+
     return logger
 
 
 if __name__ == "__main__":
-    logger = getLogger()
-    load_dotenv() # должен быть файл .env, а в нем токен
-    main()
+    logger = get_logger()
+    load_dotenv()  # должен быть файл .env, а в нем токен
+    bot = TelegramBotEmptyAudienceBMSTU()
+    bot.run()
