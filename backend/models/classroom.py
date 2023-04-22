29e2f79d4aae23faa6a326360e41cb2fc486b629
MyMiDiII <mymidi.student@yandex.ru>
2023-04-22 22:23:58 +0300

    #4: добавлена сущность аудитории

---
+++ backend/models/classroom.py
@@ -0,0 +1,18 @@
+from database import db
+
+class Classroom(db.Model):
+    __tablename__ = "classrooms"
+
+    classroom_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    building = db.Column(db.String())
+    floor = db.Column(db.Integer)
+    number = db.Column(db.String())
+
+    def __repr__(self):
+        return (
+            f"Classroom\n"
+            f"id:       {self.classroom_id}\n"
+            f"building: {self.building}\n"
+            f"floor:    {self.floor}\n"
+            f"number:   {self.number}"
+        )
