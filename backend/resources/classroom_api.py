a5d082b375463806d47e8882f2006eb2258afd13
MyMiDiII <mymidi.student@yandex.ru>
2023-04-28 15:43:44 +0300

    #17: добавлен парсинг параметров get-запроса аудиторий

--- backend/resources/classroom_api.py
+++ backend/resources/classroom_api.py
@@ -8,9 +8,15 @@ from schemes.classroom_schema import ClassroomSchema
 
 class ClassroomAPI(Resource):
     def get(self):
+        schedule_class = request.args.get("class")
+
+        building = request.args.get("building")
+        floor = int(request.args.get("floor") or -1)
+
         fields = request.args.get("fields")
         fields = fields.split(',') if fields else fields
         schema = ClassroomSchema(only=fields)
 
         classrooms = Classroom.query.all()
+
         return jsonify([schema.dump(classroom) for classroom in classrooms])
