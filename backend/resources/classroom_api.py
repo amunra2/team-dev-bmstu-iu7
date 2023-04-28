19e2b0d993ba5862192114677b08147a34ac84ef
MyMiDiII <mymidi.student@yandex.ru>
2023-04-28 16:48:59 +0300

    #17: добавлена фильтрация аудиторий по паре (поиск свободных аудиторий)

--- backend/resources/classroom_api.py
+++ backend/resources/classroom_api.py
@@ -3,6 +3,8 @@ from flask import jsonify, request
 from flask_restful import Resource
 
 from models.classroom import Classroom
+from models.schedule_class import ScheduleClass
+from models.state import State
 
 from schemes.classroom_schema import ClassroomSchema
 
@@ -24,6 +26,17 @@ class ClassroomAPI(Resource):
 
         if floor:
             classrooms = classrooms.filter_by(floor=int(floor))
+
+        if schedule_class:
+            week, day, time = map(int, schedule_class.split(','))
+
+            schedule_class = ScheduleClass.query\
+                                .filter_by(week=week, day=day, time=time)\
+                                .with_entities(ScheduleClass.class_id)
+
+            occupied_classrooms_ids = State.query.filter_by(class_id=schedule_class).with_entities(State.classroom_id)
+
+            classrooms = classrooms.filter(Classroom.classroom_id.not_in(occupied_classrooms_ids))
 
         classrooms = classrooms.all()
 
