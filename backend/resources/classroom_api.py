4345b5b1a23c3250a13cf5876067466f0e04dbe9
MyMiDiII <mymidi.student@yandex.ru>
2023-04-30 11:57:50 +0300

    #17: get-метод аудиторий декомпозирован на несколько методов

--- backend/resources/classroom_api.py
+++ backend/resources/classroom_api.py
@@ -15,30 +15,37 @@ class ClassroomAPI(Resource):
         is_free_mode = (request.args.get("is_free") == "true")
 
         if is_free_mode:
-            number = request.args.get("number")
-            schedule_class = request.args.get("class")
-            week, day, time = map(int, schedule_class.split(','))
-
-            classroom_id = Classroom.query.filter_by(number=number).with_entities(Classroom.classroom_id)
-            schedule_class_id = ScheduleClass.query\
-                                .filter_by(week=week, day=day, time=time)\
-                                .with_entities(ScheduleClass.class_id)
-
-            in_states = State.query.filter_by(classroom_id=classroom_id, class_id=schedule_class_id).all()
-
-            answer = { "is_free" : False if in_states else True }
-
-            return jsonify(answer)
-
-        schedule_class = request.args.get("class")
-
-        building = request.args.get("building")
-        floor = request.args.get("floor")
+            return self._check_free()
 
         fields = request.args.get("fields")
         fields = fields.split(',') if fields else fields
         schema = ClassroomSchema(only=fields)
 
+        building = request.args.get("building")
+        floor = request.args.get("floor")
+        schedule_class = request.args.get("class")
+
+        classrooms = self._get_by_params(building, floor, schedule_class)
+
+        return jsonify([schema.dump(classroom) for classroom in classrooms])
+
+    def _check_free(self):
+        number = request.args.get("number")
+        schedule_class = request.args.get("class")
+        week, day, time = map(int, schedule_class.split(','))
+
+        classroom_id = Classroom.query.filter_by(number=number).with_entities(Classroom.classroom_id)
+        schedule_class_id = ScheduleClass.query\
+                            .filter_by(week=week, day=day, time=time)\
+                            .with_entities(ScheduleClass.class_id)
+
+        in_states = State.query.filter_by(classroom_id=classroom_id, class_id=schedule_class_id).all()
+
+        answer = { "is_free" : False if in_states else True }
+
+        return jsonify(answer)
+
+    def _get_by_params(self, building, floor, schedule_class):
         classrooms = Classroom.query
 
         if building:
@@ -48,6 +55,11 @@ class ClassroomAPI(Resource):
             classrooms = classrooms.filter_by(floor=int(floor))
 
         if schedule_class:
+            classrooms = self._filter_by_schedule_class(schedule_class, classrooms)
+
+        return classrooms.all()
+
+    def _filter_by_schedule_class(self, schedule_class, classrooms):
             week, day, time = map(int, schedule_class.split(','))
 
             schedule_class = ScheduleClass.query\
@@ -56,8 +68,5 @@ class ClassroomAPI(Resource):
 
             occupied_classrooms_ids = State.query.filter_by(class_id=schedule_class).with_entities(State.classroom_id)
 
-            classrooms = classrooms.filter(Classroom.classroom_id.not_in(occupied_classrooms_ids))
+            return classrooms.filter(Classroom.classroom_id.not_in(occupied_classrooms_ids))
 
-        classrooms = classrooms.all()
-
-        return jsonify([schema.dump(classroom) for classroom in classrooms])
